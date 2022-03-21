51d528e639b6a81bcf0d995bef16798269b0ec44
Shohei Aoki <shoaok@gmail.com>
2022-03-21 10:54:44 +0300

    add thickness calculation

--- displacement_momentum_thickness.m
+++ displacement_momentum_thickness.m
@@ -3,7 +3,17 @@ clear; close all; clc;
 blasius;
 
 a = trapz(eta, 1-fprime);
-fprintf('Displacement thickness δ*/x=%.4f/√Rex \n',a);
+figure();
+area(eta, 1-fprime);
+xlabel('$$\eta$$','Interpreter','latex');
+ylabel('$$(1-\frac{u}{U_{\infty}}), U_{\infty}(1-\frac{u}{U_{\infty}})$$','Interpreter','latex');
+fprintf('排除厚さ Displacement thickness δ*/x=%.4f/√Rex \n',a);
 
 b = trapz(eta, fprime.*(1-fprime));
-fprintf('Momemtum thickness θ/x=%.4f/√Rex \n',b);
+hold on;
+area(eta, fprime.*(1-fprime));
+fprintf('運動量厚さ Momemtum thickness θ/x=%.4f/√Rex \n',b);
+legend('$$\left(1-\frac{u}{U_{\infty}}\right)$$', '$$U_{\infty}\left(1-\frac{u}{U_{\infty}}\right)$$','interpreter','latex');
+title('排除厚さと運動量厚さの計算');
+
+big;
